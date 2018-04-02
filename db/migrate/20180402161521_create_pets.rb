3a002bcea4b5202d5b22ab540d80efc27a9c1955
Amanda <amandarebecca89@gmail.com>
2018-04-02 12:28:06 -0400

    Created pet table

---
+++ db/migrate/20180402161521_create_pets.rb
@@ -0,0 +1,11 @@
+class CreatePets < ActiveRecord::Migration
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :family
+      t.string :zone_caught
+      t.integer :hit_chance
+      t.interger :user_id
+    end
+  end
+end
