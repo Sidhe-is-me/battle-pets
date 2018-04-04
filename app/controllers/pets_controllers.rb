22dc0b778600ee0da7c8cfbaa5257fa4c5528ef5
Amanda <amandarebecca89@gmail.com>
2018-04-04 16:23:27 -0400

    added in code for all pets to show the list of pets you have

--- app/controllers/pets_controllers.rb
+++ app/controllers/pets_controllers.rb
@@ -1,7 +1,12 @@
 class PetsController < ApplicationController
 
   get "/all_pets" do
+    @pets = Pet.all
     erb :'/pets/all_pets'
+  end
+
+  get "/create_pet" do
+    redirect to '/pets/create_pet'
   end
 
 end
