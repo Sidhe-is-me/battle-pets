b572b71528f3b41a02fc57660a0098952d70f1bf
Amanda <amandarebecca89@gmail.com>
2018-04-04 12:44:01 -0400

    trying to add in the background picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ get "/users/signup" do
   erb :'users/signup'
 end
 
+post "/users/signup" do
+       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+       redirect to '/all_pets'
+end
+
 
 
 end
